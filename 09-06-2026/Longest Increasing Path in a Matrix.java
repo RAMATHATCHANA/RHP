f13f30f2f595b31e1b20b182d5c9c391ff83aa8f
Tharshana <rthatchana.cse@gmail.com>
2026-06-10 19:25:52 +0530

    Refactor DFS for longest increasing path in matrix

---
+++ 09-06-2026/Longest Increasing Path in a Matrix.java
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    const int diff[4][2] = {
+        {0, -1}, {0, 1},
+        {-1, 0}, {1, 0}
+    };
+
+    int dfs(vector<vector<int>>& matrix,
+            vector<vector<int>>& dp,
+            int R, int C,
+            int row, int col) {
+
+        if (dp[row][col] != 0) {
+            return dp[row][col];
+        }
+        int adjMax = 0;
+        for (int i = 0; i < 4; i++) {
+
+            int ar = row + diff[i][0];
+            int ac = col + diff[i][1];
+
+            if (ar >= 0 && ar < R &&
+                ac >= 0 && ac < C &&
+                matrix[ar][ac] > matrix[row][col]) {
+
+                adjMax = max(adjMax,
+                             dfs(matrix, dp, R, C, ar, ac));
+            }
+        }
+        dp[row][col] = 1 + adjMax;
+        return dp[row][col];
+    }
+
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int R = matrix.size();
+        int C = matrix[0].size();
+        vector<vector<int>> dp(R, vector<int>(C, 0));
+        int maxLen = 1;
+        for (int row = 0; row < R; row++) {
+            for (int col = 0; col < C; col++) {
+                maxLen = max(maxLen, dfs(matrix, dp, R, C, row, col));
+            }
+        }
+
+        return maxLen;
+    }
+};
